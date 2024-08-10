44e7982617d5787c9f6c874fafa6ede4ab8b7c26
sametfurkan <59727471+sametfurkan@users.noreply.github.com>
2024-08-10 20:33:01 +0300

    start

---
+++ src/main/java/org/sfe/springbootsampletwo/SpringbootsampletwoApplication.java
@@ -0,0 +1,13 @@
+package org.sfe.springbootsampletwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsampletwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootsampletwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sfe/springbootsampletwo/SpringbootsampletwoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sfe.springbootsampletwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsampletwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
